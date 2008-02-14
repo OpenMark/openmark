965633cc5a0afa30402050a959a147653301eccb
Spencer Harben <S.M.Harben@open.ac.uk>
2008-02-14 14:38:31 +0000

    wordselect mk4

--- src/om/stdcomponent/WordSelectComponent.js
+++ src/om/stdcomponent/WordSelectComponent.js
@@ -1,5 +1,10 @@
 // to be added word select component
 
+var isKHTML=navigator.userAgent.indexOf('KHTML')!=-1;
+var isGecko=!isKHTML && navigator.userAgent.indexOf('Gecko/')!=-1;
+var isOpera=navigator.userAgent.indexOf('Opera')!=-1;
+var isIE=!isOpera && navigator.userAgent.match('.*MSIE.*Windows.*');
+
 function wordOnClick(checkboxID,idPrefix)
 {
 	var label = document.getElementById(idPrefix+'label_wordselectword_'+checkboxID);
@@ -16,14 +21,18 @@ function wordOnFocus(checkboxID,idPrefix)
 {
 	var label = document.getElementById(idPrefix+'label_wordselectword_'+checkboxID);
 	var checkbox=document.getElementById(idPrefix+'wordselectword_'+checkboxID);
+	var div = document.getElementById(idPrefix+'div_wordselectword_'+checkboxID);
 
-		label.className = label.className + " borderfocus";
+	if(isGecko){
+		div.className = div.className + " borderfocus";
+	}
 }
 
 function wordOnBlur(checkboxID,idPrefix)
 {
 	var label = document.getElementById(idPrefix+'label_wordselectword_'+checkboxID);
 	var checkbox=document.getElementById(idPrefix+'wordselectword_'+checkboxID);
+	var div=document.getElementById(idPrefix+'div_wordselectword_'+checkboxID);
 
-		label.className = label.className.replace("borderfocus", "");
+		div.className = div.className.replace("borderfocus", "");
 }
