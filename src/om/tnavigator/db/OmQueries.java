b705df4d98a209a09dfaea0cca217ef0bc490cde
Tim Hunt <T.J.Hunt@open.ac.uk>
2013-01-15 18:10:34 +0000

    Fix failing unit test.

--- src/om/tnavigator/db/OmQueries.java
+++ src/om/tnavigator/db/OmQueries.java
@@ -1131,7 +1131,7 @@ public abstract class OmQueries
 
 	public String retrieveAuthorshipConfirmationQuery() {
 		return "SELECT authorshipConfirmation from "
-			+ getPrefix() + "tests where ti = {0}";
+			+ getPrefix() + "tests WHERE ti = {0}";
 	}
 
 
