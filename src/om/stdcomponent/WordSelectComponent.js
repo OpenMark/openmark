ee55a2eee4a34a2659fc7293113dd6a831a93d99
Tim Hunt <T.J.Hunt@open.ac.uk>
2013-10-16 16:38:24 +0100

    Bug 12191 Strip out legacy IE code in wordselect.
    The standard code works fine in IE8, 9 and 10.

--- src/om/stdcomponent/WordSelectComponent.js
+++ src/om/stdcomponent/WordSelectComponent.js
@@ -1,5 +1,5 @@
 // to be added word select component
-function wordOnClick(checkboxID,idPrefix)
+function wordOnClick(checkboxID, idPrefix)
 {
 	var label = document.getElementById(idPrefix+'label_wordselectword_'+checkboxID);
 	var checkbox = document.getElementById(idPrefix+'wordselectword_'+checkboxID);
@@ -16,33 +16,29 @@ function wordOnClick(checkboxID,idPrefix)
 	}
 }
 
-function wordOnFocus(checkboxID,idPrefix)
+function wordOnFocus(checkboxID, idPrefix)
 {
 	var div = document.getElementById(idPrefix+'div_wordselectword_'+checkboxID);	
 	// Firefox, IE8 and other browsers don't automatically create a focus rectangle so have to draw one in.
 	// IE7 and below versions do create a focus rectangle automatically.
-	if(!isIE7OrBelow){
-		div.className = div.className + " borderfocus";
-	}
+	div.className = div.className + " borderfocus";
 	if (isKHTML) {
 		// Fix some Chrome layout glitches.
 		khtmlForceRepaint(div);
 	}
 }
 
-function wordGeckoBorder(checkboxID,idPrefix)
+function wordGeckoBorder(checkboxID, idPrefix)
 {
 	// This is called onLoad and ulimately adds a transparent border around the
 	//the displayed word. This stops the words from moving around as the focus
 	//rectangle moves. The border cannot be added directly as any left or right
 	//borders stuff the layout of the component in IE7 or below
 	var div = document.getElementById(idPrefix+'div_wordselectword_'+checkboxID);
-	if(!isIE7OrBelow){
-		div.className = div.className + " geckoselectworddiv";
-	}
+	div.className = div.className + " geckoselectworddiv";
 }
 
-function wordOnBlur(checkboxID,idPrefix)
+function wordOnBlur(checkboxID, idPrefix)
 {
 	var div = document.getElementById(idPrefix+'div_wordselectword_'+checkboxID);
 	div.className = div.className.replace(" borderfocus", "");
